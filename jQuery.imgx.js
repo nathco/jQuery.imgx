5698a0e9ac38d61bbde36f52618fdb8be73f40fd
Eli Slade <elislade@gmail.com>
2015-03-05 10:03:24 -0700

    Reorganized x2, x3 and abstracted dpi function
    Move x2 and x3 outside of the each function to improve performance so you don't call them again & again for each image. Also abstracted your dpi stuff into a function.

--- jQuery.imgx.js
+++ jQuery.imgx.js
@@ -5,37 +5,35 @@
 (function($) {
 
     $.fn.imgx = function() {
-    
+        
+        var x2, x3;
+        
+        var dpi = function(density){
+            return  '(-webkit-min-device-pixel-ratio:'+ density +')'+
+                    '(-o-min-device-pixel-ratio: '+ density +'/1)'+
+                    '(min--moz-device-pixel-ratio: '+ density +')'+
+                    '(min-device-pixel-ratio: '+ density +')'+
+                    '(min-resolution: '+ density +'dppx)';
+        };
+            
+        x2 = function() {
+            
+            return 1 < window.devicePixelRatio ||
+                       window.matchMedia &&
+                       window.matchMedia(dpi(2)).matches ? !0 : !1;
+        }();
+            
+        x3 = function() {
+                             
+            return 2 < window.devicePixelRatio ||
+                       window.matchMedia &&
+                       window.matchMedia(dpi(3)).matches ? !0 : !1;
+        }();
+            
         return this.each(function() {
         
-            var src, x2, x3;
-            
-            x2 = function() {
-            
-                var dpi = '(-webkit-min-device-pixel-ratio: 2)'+
-                          '(-o-min-device-pixel-ratio: 2/1)'+
-                          '(min--moz-device-pixel-ratio: 2)'+
-                          '(min-device-pixel-ratio: 2)'+
-                          '(min-resolution: 2dppx)';
-                             
-                return 1 < window.devicePixelRatio ||
-                           window.matchMedia &&
-                           window.matchMedia(dpi).matches ? !0 : !1;
-            }();
-            
-            x3 = function() {
+            var src;
 
-                var dpi = '(-webkit-min-device-pixel-ratio: 3)'+
-                          '(-o-min-device-pixel-ratio: 3/1)'+
-                          '(min--moz-device-pixel-ratio: 3)'+
-                          '(min-device-pixel-ratio: 3)'+
-                          '(min-resolution: 3dppx)';
-                             
-                return 2 < window.devicePixelRatio ||
-                           window.matchMedia &&
-                           window.matchMedia(dpi).matches ? !0 : !1;
-            }();
-            
             if (x2) src = $(this).attr('src').replace(/\.(gif|jpg|jpeg|png)$/,'@2x.$1');
             if (x3) src = $(this).attr('src').replace(/\.(gif|jpg|jpeg|png)$/,'@3x.$1');
                           $(this).attr('src', src);
